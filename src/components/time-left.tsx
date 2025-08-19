6e975a066218b2bcf1722f9d60c396632c5fe6be
Marcone Boff <157663360+m4rcone@users.noreply.github.com>
2025-08-19 14:58:45 -0300

    fix: time-left component

--- src/components/time-left.tsx
+++ src/components/time-left.tsx
@@ -8,73 +8,61 @@ type TimeLeft = {
 };
 
 export function TimeLeft({ expiresAt }: { expiresAt: string }) {
+  const [mounted, setMounted] = useState(false);
   const [timeLeft, setTimeLeft] = useState<TimeLeft | null>(null);
 
   function calculateTimeLeft(expiresAt: string): TimeLeft | null {
-    const expiresAtDate = new Date(expiresAt);
-    const now = new Date();
-    const differenceInMillisencods = expiresAtDate.getTime() - now.getTime();
+    const expiresAtDate = new Date(expiresAt).getTime();
+    const now = Date.now();
+
+    const diff = Math.max(0, expiresAtDate - now);
+
+    if (diff <= 0) {
+      return null;
+    }
 
     const dayInMilliseconds = 1000 * 60 * 60 * 24;
     const hourInMilliseconds = 1000 * 60 * 60;
     const minuteInMilliseconds = 1000 * 60;
 
-    if (differenceInMillisencods <= 0) {
-      return null;
-    }
-
-    const hours = Math.floor(
-      (differenceInMillisencods % dayInMilliseconds) / hourInMilliseconds,
-    );
+    const hours = Math.floor((diff % dayInMilliseconds) / hourInMilliseconds);
     const minutes = Math.floor(
-      (differenceInMillisencods % hourInMilliseconds) / minuteInMilliseconds,
+      (diff % hourInMilliseconds) / minuteInMilliseconds
     );
-    const seconds = Math.floor(
-      (differenceInMillisencods % minuteInMilliseconds) / 1000,
-    );
+    const seconds = Math.floor((diff % minuteInMilliseconds) / 1000);
 
     return { hours, minutes, seconds };
   }
 
-  function formatTimeLeft(timeLeft: TimeLeft): string {
-    const parts = [];
-
-    if (timeLeft.hours > 0) {
-      parts.push(`${timeLeft.hours}h`);
-    }
-
-    if (timeLeft.minutes > 0) {
-      parts.push(`${timeLeft.minutes}m`);
-    }
-
-    if (timeLeft.seconds > 0) {
-      parts.push(`${timeLeft.seconds}s`);
-    }
-
-    return parts.join(" ");
-  }
-
   useEffect(() => {
-    const timer = setInterval(() => {
+    setMounted(true);
+    let timeout: NodeJS.Timeout;
+
+    function tick() {
       const remaining = calculateTimeLeft(expiresAt);
+      setTimeLeft(remaining);
 
-      if (!remaining) {
-        clearInterval(timer);
-      } else {
-        setTimeLeft(remaining);
+      if (remaining) {
+        timeout = setTimeout(tick, 1000);
       }
-    }, 1000);
+    }
 
-    const initialTime = calculateTimeLeft(expiresAt);
-    setTimeLeft(initialTime);
+    tick();
 
-    return () => clearInterval(timer);
+    return () => clearTimeout(timeout);
   }, [expiresAt]);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <div className="flex items-center justify-center gap-2">
       <Clock width={16} />
-      <p className="italic">{`${timeLeft && formatTimeLeft(timeLeft)}`}</p>
+      <p className="italic">
+        {timeLeft &&
+          `${timeLeft.hours}h ${timeLeft.minutes}m ${timeLeft.seconds}s`}
+      </p>
     </div>
   );
 }
